ed2359dbd265bfdb517d668f7357ab84fc1dc2f0
LD MacKrell <ldmackrell@gmail.com>
2017-09-29 16:48:24 -0700

    update app to prevent duplicate entries for stores or brands.

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,3 +1,9 @@
 class Brand < ActiveRecord:: Base
   has_and_belongs_to_many :stores
+
+  validates(:brandname, {
+    :presence => true,
+    :uniqueness => { case_sensitive: false},
+    :length => { :maximum => 100 }
+    })
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,9 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many :brands
+
+  validates(:name, {
+    :presence => true,
+    :uniqueness => { case_sensitive: false},
+    :length => { :maximum => 100 }
+    })
 end
